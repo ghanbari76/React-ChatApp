2ee944fdb1662a119f4954d3a4eb32607eba2133
Javad Ghanbari <javadghanbari.put@gmail.com>
2023-07-08 18:58:01 -0700

    Adding Login Page to app

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,9 +1,14 @@
+import { Route,Switch } from 'react-router-dom';
 import './App.css';
+//Components 
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      Chat App
+      <Switch>
+        <Route path="/" component={Login} />
+      </Switch>
     </div>
   );
 }
