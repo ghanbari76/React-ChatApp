c1aea823d1bb818ddd66c4a6338f219b9bbeebdc
Javad Ghanbari <javadghanbari.put@gmail.com>
2023-07-10 12:24:06 -0700

    Added logoutHandler

--- chat-app/src/components/Chats.js
+++ chat-app/src/components/Chats.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { auth } from '../firebase';
+import { useHistory } from 'react-router-dom';
 //Components
 import Navbar from './Navbar';
 //Styles
 import styles from './Chats.module.css';
 
 const Chats = () => {
+    const history = useHistory();
+    const logoutHandler = async () => {
+        await auth.signOut();
+        history.push("/")
+    }
     return (
         <div className={styles.container}>
-            <Navbar />
+            <Navbar logoutHandler={logoutHandler} />
         </div>
     );
 };
